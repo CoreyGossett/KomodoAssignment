37377ca5de82b420e935e8a161540390d9996545
Corey Gossett <coreymgossett@gmail.com>
2021-12-03 14:04:27 -0500

    Added UI Menu and Run Methods

--- KomodoInsurance.UI/ProgramUI.cs
+++ KomodoInsurance.UI/ProgramUI.cs
@@ -3,10 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KomodoInsurance.Repository;
 
 namespace KomodoInsurance.UI
 {
     class ProgramUI
     {
+        private readonly DevTeamRepo _devTeamRepo = new DevTeamRepo();
+
+        private readonly DeveloperRepo _devRepo = new DeveloperRepo();
+
+        internal void Run()
+        {
+            RunApplication();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Welcome to Komodo Insurance Team Manager! What can we do for you today?\n" +
+                "1. Add Developer\n" +
+                "2. Create Team\n" +
+                "3. Add Developer To Team\n" +
+                "4. View Developers\n" +
+                "5. View Teams\n" +
+                "6. Exit Application");
+        }
+
+        private void RunApplication()
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Clear();
+                Menu();
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        AddDeveloper();
+                        break;
+                    case "2":
+                        CreateTeam();
+                        break;
+                    case "3":
+                        AddDeveloperToTeam();
+                        break;
+                    case "4":
+                        ViewAllDevelopers();
+                        break;
+                    case "5":
+                        ViewAllTeams();
+                        break;
+                    case "6":
+                        isRunning = false;
+                        Console.WriteLine("Thank you for using Komodo's Team Manager! Have a great day!");
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+
+        private void ViewAllTeams()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ViewAllDevelopers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddDeveloperToTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddDeveloper()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
